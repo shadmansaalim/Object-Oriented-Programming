ef46d6aea21b4673f5b2009c429aefc6dec47cae
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-16 15:39:22 +1100

    Class writing in Python and making an object using that class

---
+++ day1/phone.py
@@ -0,0 +1,12 @@
+class Phone:
+    brand = 'Apple'
+    color = 'Purple'
+    price = 1899
+
+
+# Creating an object of Phone class
+my_phone = Phone()
+print(my_phone)
+
+dad_phone = Phone()
+mom_phone = Phone()
