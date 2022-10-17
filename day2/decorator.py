248dcfb3c77616c128954b5da499bbb00ca43786
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-17 23:49:37 +1100

    Understanding Decorator

---
+++ day2/decorator.py
@@ -0,0 +1,27 @@
+import math
+import time
+
+
+def timer(func):
+    def inner(*args, **kwargs):
+        print('Time Start')
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print(f'Time End : Time Took For Execution {end-start}')
+    return inner
+
+
+@timer
+def get_factorial(n):
+    result = math.factorial(n)
+    print(f'Factorial of {n} is {result}')
+
+
+# timer(get_factorial)()
+
+# Working because of *args
+# get_factorial(5)
+
+# We can do this only because in the above inner function inside timmer function we wrote **kwargs as argument which basically means multiple keyword arguments
+get_factorial(n=5)
