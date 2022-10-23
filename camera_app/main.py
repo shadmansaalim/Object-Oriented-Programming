516a556d934e6d4154e210764ea46754af48e30e
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-23 16:47:31 +1100

    Init of camera app started, will continue later

---
+++ camera_app/main.py
@@ -0,0 +1,14 @@
+"""
+    My Camera Application
+    Author : Saalim Shadman
+"""
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap, QImage, QIcon
+import cv2
+
+
+class Window(QWidget):
+    """ Main App Window """
+
+    def __init
