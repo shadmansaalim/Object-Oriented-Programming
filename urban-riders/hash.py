b3311fc76b916e20dc543a989720e1b99cd0f15b
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-30 18:59:58 +1100

    Password hashing using hashlib

--- urban-riders/hash.py
+++ urban-riders/hash.py
@@ -1,2 +1,13 @@
+"""Encryption example"""
 # Rtrams -> smart
 # Splkiyoanr -> lion
+
+import hashlib
+
+email = 'coder@gmail.com'
+pwd = 'abc123'
+pwd_encode = pwd.encode()
+pwd_hash = hashlib.md5(pwd_encode).hexdigest()
+
+print(pwd)
+print(pwd_hash)
