e46c29c4c6af7e0a78f4e06777429241ea15fc9b
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-30 19:18:37 +1100

    Login static method built with validation

--- urban-riders/user.py
+++ urban-riders/user.py
@@ -11,5 +11,26 @@ class User:
         file.close()
         print(f"{email} User Created Successfully")
 
+    @staticmethod
+    def login(email, password):
+        stored_pass = ""
+        with open('users.text', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                if email in line:
+                    stored_pass = line.split(' ')[1]
+                    break
+        file.close()
+        if (len(stored_pass)):
+            hashed_pwd = hashlib.md5(password.encode()).hexdigest()
+            if (hashed_pwd == stored_pass):
+                print(f"Successfully Logged In {email}")
+            else:
+                print("Invalid Password")
+        else:
+            print(f"No user exists with email {email}")
+
 
 saalim = User("Saalim Shadman", "abc123@gmail.com", "123456@")
+
+User.login("abc123@gmail.com", "123456@")
