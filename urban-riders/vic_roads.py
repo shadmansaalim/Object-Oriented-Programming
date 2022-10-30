106ffc976a8be33ce70e43a789a85ff57c2db666
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-30 20:04:33 +1100

    Vicroads Transport authority class built with methods

---
+++ urban-riders/vic_roads.py
@@ -0,0 +1,29 @@
+import random
+
+
+# Transport Authority
+class VicRoads:
+    def __init__(self) -> None:
+        self.__license = {}
+
+    def driving_test(self, email):
+        score = random.randint(0, 100)
+        if score > 30:
+            print("Driving test passed successfully")
+            license_number = random.randint(5000, 9999)
+            self.__license[email] = license_number
+            return license_number
+        else:
+            print("FAILED! Driving Test")
+            return False
+
+    def validate_license(self, email, license):
+        if email in self.__license:
+            return self.__license[email] == license
+        return False
+
+
+vic = VicRoads()
+
+license = vic.driving_test("abc123@gmail.com")
+print(vic.validate_license("abc123@gmail.com", license))
