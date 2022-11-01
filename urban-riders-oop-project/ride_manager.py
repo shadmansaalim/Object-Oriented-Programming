12d6f6a78f523aad4a7aa3d6c68d4e6b3189884e
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-01 19:33:29 +1100

    Code improved reduced

--- urban-riders-oop-project/ride_manager.py
+++ urban-riders-oop-project/ride_manager.py
@@ -18,18 +18,14 @@ class RideManager:
         for vehicle in self.__available_vehicles[vehicle_type]:
             passenger_location = passenger.get_location()
             vehicle_location = vehicle.driver.location
-            if abs(passenger_location - vehicle_location) < 30:
-                if vehicle.available == True:
-                    # Making the vehicle unavailable
-                    vehicle.available = False
-                    # Removing the vehicle from available list
-                    print(f'Available {vehicle_type}s',
-                          len(self.__available_vehicles[vehicle_type]))
-                    self.__available_vehicles[vehicle_type].remove(vehicle)
-                    print(f'Available {vehicle_type}s',
-                          len(self.__available_vehicles[vehicle_type]))
-                    print(f"Found a {vehicle_type} match for you")
-                    return True
+            distance = abs(passenger_location - vehicle_location)
+            if distance < 30 and vehicle.available == True:
+                # Making the vehicle unavailable
+                vehicle.available = False
+                # Removing the vehicle from available list
+                self.__available_vehicles[vehicle_type].remove(vehicle)
+                print(f"Found a {vehicle_type} match for you")
+                return True
 
 
 uber = RideManager()
