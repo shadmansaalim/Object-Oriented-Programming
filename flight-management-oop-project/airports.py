2cd28c54505c591fd68981e5a15b8d0cbce2f798
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-07 20:33:08 +1100

    Complex data retrieval for currency rate achieved

--- flight-management-oop-project/airports.py
+++ flight-management-oop-project/airports.py
@@ -9,6 +9,26 @@ class Airports:
 
     def load_airport_data(self, file_path):
         airports = {}
+        currency_rates = {}
+        country_currency = {}
+
+        with open('./data/currencyrates.csv', 'r') as file:
+            lines = csv.reader(file)
+            for line in lines:
+                currency_short_form = line[1]
+                currency_rate = line[2]
+                currency_rates[currency_short_form] = currency_rate
+        file.close()
+
+        with open('./data/countrycurrency.csv', 'r') as file:
+            lines = csv.reader(file)
+            for line in lines:
+                country_name = line[0]
+                currency_alphabetic_code = line[1]
+                country_currency[country_name] = currency_alphabetic_code
+        file.close()
+
+        # Creation of Airport
         with open(file_path, 'r', encoding="utf8") as file:
             lines = csv.reader(file)
             for line in lines:
@@ -18,9 +38,15 @@ class Airports:
                 airport_country = line[3]
                 airport_lat = line[6]
                 airport_long = line[7]
-                airport_rate = 0
+
+                currency = country_currency[airport_country]
+                airport_rate = currency_rates[currency]
+
                 # Using airport_code as dictionary key for each airport as the it is unique
                 airports[airport_code] = Airport(
                     airport_code, airport_name, airport_city, airport_country, airport_lat, airport_long, airport_rate)
         self.airports = airports
         file.close()
+
+
+Airports()
