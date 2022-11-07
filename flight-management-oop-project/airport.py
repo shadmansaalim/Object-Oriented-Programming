f9984df76c8ec58279b675a772307ebb3c680778
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-07 17:40:00 +1100

    Private attribute and getter

--- flight-management-oop-project/airport.py
+++ flight-management-oop-project/airport.py
@@ -1,10 +1,16 @@
 class Airport:
     def __init__(self, name, country, lat, long, rate) -> None:
+        # Public Attributes
         self.name = name
         self.country = country
         self.lat = lat
         self.long = long
-        self.rate = rate
+        # Private
+        self.__rate = rate
+
+    # Getter
+    def get_rate(self):
+        return self.__rate
 
     # Dunder
     def __repr__(self) -> str:
