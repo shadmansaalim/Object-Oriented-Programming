ae6b2413b88a99dd1486334a59eec770fd445d5f
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-07 17:18:26 +1100

    Flght management/scheduling system OOP project started, aircraft class init

---
+++ flight-management-oop-project/aircraft.py
@@ -0,0 +1,6 @@
+class Aircraft:
+    def __init__(self, make, code, category, flight_range) -> None:
+        self.make = make
+        self.code = code
+        self.category = category
+        self.flight_range = flight_range
