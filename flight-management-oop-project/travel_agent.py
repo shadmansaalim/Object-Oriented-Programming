692e9e6be52f9821f074ee3ec67f16b3f4a1e37d
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-11 17:26:21 +0600

    Travel agent behaviours method added

--- flight-management-oop-project/travel_agent.py
+++ flight-management-oop-project/travel_agent.py
@@ -3,5 +3,17 @@ class TravelAgent:
         self.name = name
         self.trips = None
 
+    def set_trip_one_city_one_way(self):
+        pass
+
+    def set_trip_one_city_two_way(self):
+        pass
+
+    def set_trip_multi_city_one_way(self):
+        pass
+
+    def set_trip_multi_city_round(self):
+        pass
+
     def __repr__(self) -> str:
         return f'TravelAgent: {self.name}'
