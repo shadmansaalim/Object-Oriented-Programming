1e4cb395566d95926f7134aae86d2428741eb87e
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-12 17:59:09 +0600

    Uber project main file standerised and code quality improved

--- uber-oop-project/main.py
+++ uber-oop-project/main.py
@@ -2,33 +2,39 @@ from random import randint, choice
 from ride_manager import uber
 from user import Passenger, Driver
 
-# Creating Passenger Instances
-passenger1 = Passenger("Passenger1", "passenger1@gmail.com",
-                       "abc1", randint(0, 40), randint(1, 1000))
-passenger2 = Passenger("Passenger2", "passenger2@gmail.com",
-                       "abc2", randint(0, 40), randint(1, 1000))
 
-passenger3 = Passenger("Passenger3", "passenger3@gmail.com",
-                       "abc3", randint(0, 40), randint(1, 1000))
+def main():
+    # Creating Passenger Instances
+    passenger1 = Passenger("Passenger1", "passenger1@gmail.com",
+                           "abc1", randint(0, 40), randint(1, 1000))
+    passenger2 = Passenger("Passenger2", "passenger2@gmail.com",
+                           "abc2", randint(0, 40), randint(1, 1000))
+
+    passenger3 = Passenger("Passenger3", "passenger3@gmail.com",
+                           "abc3", randint(0, 40), randint(1, 1000))
+
+    # Creating Driver Instances
+    for i in range(1, 100):
+        driver1 = Driver(f"Driver{i}", f"driver{i}@gmail.com",
+                         f"drive{i}", randint(0, 100), randint(1000, 9999))
+        driver1.driving_test()
+        driver1.register_vehicle(
+            choice(['car', 'bike', 'tram']), randint(10000, 99999), 10)
+
+    # print(uber.get_available_vehicles('car'))
+    # print(uber.get_available_vehicles('bike'))
+    # print(uber.get_available_vehicles('tram'))
+
+    uber.find_vehicle(passenger1, choice(
+        ['car', 'bike', 'tram']), randint(1, 200))
+    uber.find_vehicle(passenger1, choice(
+        ['car', 'bike', 'tram']), randint(1, 200))
+    uber.find_vehicle(passenger1, choice(
+        ['car', 'bike', 'tram']), randint(1, 200))
+
+    print(passenger1.get_trip_history())
+    print(uber.total_income())
 
 
-# Creating Driver Instances
-for i in range(1, 100):
-    driver1 = Driver(f"Driver{i}", f"driver{i}@gmail.com",
-                     f"drive{i}", randint(0, 100), randint(1000, 9999))
-    driver1.driving_test()
-    driver1.register_vehicle(
-        choice(['car', 'bike', 'tram']), randint(10000, 99999), 10)
-
-
-# print(uber.get_available_vehicles('car'))
-# print(uber.get_available_vehicles('bike'))
-# print(uber.get_available_vehicles('tram'))
-
-uber.find_vehicle(passenger1, choice(['car', 'bike', 'tram']), randint(1, 200))
-uber.find_vehicle(passenger1, choice(['car', 'bike', 'tram']), randint(1, 200))
-uber.find_vehicle(passenger1, choice(['car', 'bike', 'tram']), randint(1, 200))
-
-
-print(passenger1.get_trip_history())
-print(uber.total_income())
+if __name__ == '__main__':
+    main()
