323cfe312d5a3405b6dc948ec59f4502ff44aa8a
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-11-19 13:13:33 +0600

    Singleton Design Pattern

---
+++ design-patterns/singleton.py
@@ -0,0 +1,28 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self) -> None:
+        if Singleton.__instance is None:
+            Singleton.__instance = self
+        else:
+            raise Exception("This is Singleton. Already have an instance")
+
+    @staticmethod
+    def get_instance():
+        if Singleton.__instance is None:
+            Singleton()
+        return Singleton.__instance
+
+# Will throw errors
+# first = Singleton()
+# second = Singleton()
+# third = Singleton()
+
+
+first = Singleton.get_instance()
+second = Singleton.get_instance()
+third = Singleton.get_instance()
+
+print(first)
+print(second)
+print(third)
